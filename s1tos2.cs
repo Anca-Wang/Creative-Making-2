4bba7de6365bb034869cc9b94593a37c2abf00ea
Qijing <115659964+Anca-Wang@users.noreply.github.com>
2023-06-12 16:54:22 +0100

    Scene shift

---
+++ s1tos2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class s1tos2 : MonoBehaviour
+{
+
+    public void Jump(){
+        SceneManager.LoadScene(2);
+    }
+    
+}
